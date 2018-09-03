f0bc6b9be6b23b280131a5eedc4e38c8356e2367
kpiaskowski <kar.piaskowski@gmail.com>
2018-09-03 03:28:48 +0200

    todo

--- mnist_my.py
+++ mnist_my.py
@@ -315,3 +315,17 @@ def main(unused_argv):
 
 if __name__ == '__main__':
     tf.app.run()
+
+# todo
+"""
+wyczysc komentarze stad
+przenies utilsy (cosinize itp) do osobnego miejsca
+wez jakos model opakuj ladnie w klase
+popraw uneta zeby ladniej wygladal
+wyczysc mnist my z niepotrzebnych funkcji
+wyczysc projekt z niepotrzenych plikow
+przenies reshapery do jakiegos innego, ladnego miejca (moze set_shape)??
+opakuj ladnie sciezki do logow i data: (moze clusterone gety??
+sprawdz, czy jest cos takiego jak clusterone data..
+sprawdz, czy moze jest juz dostep do datasetu
+"""
